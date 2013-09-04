81608e4f84ef72d1cf1641723cc9b2f8c7d65d96
Rusty Shackleford <gigolo@hotmail.co.uk>
2013-09-05 00:41:08 +0100

    Added ability to get newest and random to dandy.pl

--- dandy.pl
+++ dandy.pl
@@ -8,8 +8,6 @@ use open ':std', ':encoding(UTF-8)';
 use LWP::Simple;
 use HTML::TreeBuilder 5 -weak;
 
-my $base_url = 'http://www.choi-waru.com/title/index_search.php?number=';
-
 my $prog = '';
 if ($prog eq '') {
     given ($^O) {
@@ -20,19 +18,22 @@ if ($prog eq '') {
     die "What OS are you even using? It's 2013, you know?" if $prog eq '';
 }
 
-my $tree = HTML::TreeBuilder->new;
-foreach (@ARGV) {
-    die "\"$_\" is not a number!" unless ($_ =~ /^(\d+)$/);
-    my $content = get "$base_url$_" or die "ERROR! Failed to fetch URL!";
+sub get_dandy {
+    my $url = 'http://www.choi-waru.com/title/index_search.php?number=';
+    my $id  = $_[0];
+       $id  = sprintf('%03d', $id);
+
+    my $content = get "$url$id" or die "ERROR! Failed to fetch URL!";
+    my $tree = HTML::TreeBuilder->new;
     $tree->parse($content);
 
     my $new_url = '';
     for my $link ($tree->look_down(_tag => 'a')) {
         my $href = $link->attr('href');
-        $new_url = $href if $href =~ /^http:\/\/www\.choi-waru\.com\/title\/(\d+){4}\/(\d+){2}\/dandy_$_\.html$/;
+        $new_url = $href if $href =~ /^http:\/\/www\.choi-waru\.com\/title\/(\d+){4}\/(\d+){2}\/dandy_$id\.html$/;
         last unless $new_url eq '';
     }
-    die "ERROR! DANDY-$_ not found!" if $new_url eq '';
+    die "ERROR! DANDY-$id not found!" if $new_url eq '';
 
     $content = get $new_url or die "ERROR! Failed to fetch URL!";
     $tree->parse($content);
@@ -49,10 +50,36 @@ foreach (@ARGV) {
        $day_suffix  = 'st' if $date_match[1] eq "1";
        $day_suffix  = 'nd' if $date_match[1] eq "2";
 
-    print "TITLE:\n$title\n\nDESCRIPTION:\n$desc\n\nINFO:\n  $info[0]   / Director: $info[1]\n  $info[2] / Release:  $info[3] ($date_match[1]$day_suffix $date_month)\n  $info[4]   / Length:   $info[5] (minutes)\n  $info[6]   / Number:   $info[7]\n  $info[8]   / Price:    $info[9]\n";
-    my $out = qx/$prog http:\/\/www.choi-waru.com\/title\/jacket_l\/dandy_$_.jpg/;
+    print "DANDY-$id TITLE:\n$title\n\nDESCRIPTION:\n$desc\n\nINFO:\n  $info[0]   / Director: $info[1]\n  $info[2] / Release:  $info[3] ($date_match[1]$day_suffix $date_month)\n  $info[4]   / Length:   $info[5] (minutes)\n  $info[6]   / Number:   $info[7]\n  $info[8]   / Price:    $info[9]\n";
+    my $out = qx/$prog http:\/\/www.choi-waru.com\/title\/jacket_l\/dandy_$id.jpg/;
 
-    print "\n\n" unless ($_ == $ARGV[$#ARGV]);
+    $tree->delete;
 }
-$tree->delete;
+
+sub newest_dandy {
+    my $content = get "http://www.choi-waru.com/title/index.php?page=1" or die "ERROR! Failed to fetch URL!";
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($content);
+
+    my $id = 0;
+    foreach ($tree->look_down(_tag => 'a')) {
+        if ($_->attr('href') =~ /^http:\/\/www\.choi-waru\.com\/title\/(\d+){4}\/(\d+){2}\/dandy_(\d+)\.html$/) {
+            my $new_id = int($3);
+            $id = $new_id if $new_id > $id;
+        }
+    }
+
+    return $id;
+}
+
+sub random_dandy {
+    return int(rand(newest_dandy())) + 1;
+}
+
+given (lc($ARGV[0])) {
+    when('newest')  { get_dandy(newest_dandy()) }
+    when('random')  { get_dandy(random_dandy()) }
+    when(/^(\d+)$/) { get_dandy($ARGV[0])       }
+    default         { die "ERROR! Invalid argument \"$ARGV[0]\"!" }
+}
 
